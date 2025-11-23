98472b5b2dc5f7674d1ac0fa46e9c28cd37193f0
Kirasungjinwoo <kirasungjinwoo@gmail.com>
2025-11-23 12:55:27 +0530

    Add files via upload

---
+++ Number Guessing Game.py
@@ -0,0 +1,15 @@
+import random
+num = random.randint(0, 100)
+guess = int(input("Guess the number:"))
+count = 0
+while guess != num:
+    if guess < num:
+        print("Too low!")
+    elif guess > num:
+        print("Too high!")
+    count += 1
+    guess = int(input("Guess the number:"))
+if count == 1:
+    print("Correct! The number is", num, "! You guessed it in a single try!")
+elif count > 1:
+    print("Correct! The number is", num, "! You guessed it in", count, "tries!")
